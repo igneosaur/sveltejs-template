9edf388c31d8de11e9c1f47e5aacec530dc5bd2a
Dan Edwards <igneosaur@gmail.com>
2019-08-30 22:25:09 +0100

    Add await block into application
    Recreates a bug in the build process resulting in breaking JS error.

---
+++ src/Await.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let name;
+
+	const waitOneSec = () =>
+		new Promise(resolve => setTimeout(() => resolve(true), 1000));
+	const asyncCall = async () => await waitOneSec();
+
+	const data = asyncCall();
+</script>
+
+{#await data}
+	<p>Awaiting...</p>
+{:then data}
+	<h1>Hello {name}!</h1>
+{:catch error}
+	<p>Error</p>
+{/await}
